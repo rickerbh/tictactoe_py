998eca512755a6a84d705f93a579c839810567cb
Hamish Rickerby <hamish@happtic.com>
2016-07-19 22:34:17 +1000

    Start winner finding algorithm
    It seems a bit naive. Nearly there though.

--- tests/game_board_tests.py
+++ tests/game_board_tests.py
@@ -42,3 +42,17 @@ def game_board_raises_exception_when_playing_a_position_already_played_test():
         ex1 = ex
 
     assert_equal('2 is already taken', ex1.args[0])
+
+def game_is_won_on_a_row_test():
+    board = GameBoard()
+    board.play_move("X", 3)
+    assert_equal(None, board.winner())
+    board.play_move("X", 4)
+    assert_equal(None, board.winner())
+    board.play_move("X", 5)
+    assert_equal(True, board.has_winner())
+    assert_equal("X", board.winner())
+
+def game_raises_exception_when_making_a_move_and_the_game_is_won():
+    assert_equal(False, "Complete me")
+ 
--- tictactoe/game_board.py
+++ tictactoe/game_board.py
@@ -11,7 +11,9 @@ class GameBoard():
             raise ValueError("{0} is not a valid board position".format(position))
         if not self._is_position_open(position):
             raise ValueError("{0} is already taken".format(position))
-
+        if self.has_winner():
+            raise ValueError("Game is over")
+        
         self._board[position] = player
 
     def _is_position_valid(self, position):
@@ -19,5 +21,38 @@ class GameBoard():
         
     def _is_position_open(self, position):
         return self.game_state[position] == ""
+
+    def has_winner(self):
+        return any([self._row_check(), self._column_check()])
+
+    def _row_check(self):
+        return self.check_items(self._rows)
     
- 
+    @property
+    def _rows(self):
+        return list(map(self._row, list(range(3))))
+    
+    def _row(self, number):
+        start_cell = number * 3
+        return self._board[start_cell:start_cell + 3] 
+
+    def check_items(self, items):
+        checked_items = map(self._all_same, items)
+        return any(checked_items)
+    
+    def _all_same(self, items):
+        return len(set(items)) == 1 and not items[0] == ""
+
+    def _column_check(self):
+        return self.check_items(self._columns)
+    
+    @property
+    def _columns(self):
+        return list(map(self._column, list(range(3))))
+    
+    def _column(self, number):
+        return [self._board[i] for i in [number, number + 3, number + 6]]
+
+    def winner(self):
+    # get rows, test if all same, if so, return first item from matching array
+        return None
